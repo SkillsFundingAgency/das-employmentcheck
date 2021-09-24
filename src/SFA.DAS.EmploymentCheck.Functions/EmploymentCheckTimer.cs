e068a8be9a2b909cbc3c3e590a54ba9d10f79dc4
Michael Young <mick.young1981@googlemail.com>
2021-09-24 10:18:32 +0100

    Call query from function.

--- src/SFA.DAS.EmploymentCheck.Functions/EmploymentCheckTimer.cs
+++ src/SFA.DAS.EmploymentCheck.Functions/EmploymentCheckTimer.cs
@@ -1,14 +1,30 @@
 using System;
+using System.Threading.Tasks;
+using MediatR;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using SFA.DAS.EmploymentCheck.Functions.Queries.GetApprenticesToVerify;
 
 namespace SFA.DAS.EmploymentCheck.Functions
 {
-    public static class EmploymentCheckTimer
+    public class EmploymentCheckTimer
     {
-        [FunctionName(nameof(EmploymentCheckTimer))]
-        public static void Run([TimerTrigger("0 0 * * * *", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
+        private readonly IMediator _mediator;
+
+        public EmploymentCheckTimer(IMediator mediator)
         {
+            _mediator = mediator;
+        }
+
+        [FunctionName(nameof(EmploymentCheckTimer))]
+        public async Task Run([TimerTrigger("0 0 * * * *", RunOnStartup = true)]TimerInfo myTimer, ILogger log)
+        {
+            var queryResult = await _mediator.Send(new GetApprenticesToVerifyRequest());
+            foreach (var result in queryResult.ApprenticesToVerify)
+            {
+                log.LogInformation("ULN: " + result.ULN);
+            }
+            
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
     }
