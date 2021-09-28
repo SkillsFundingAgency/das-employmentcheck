7f7460690783490d43b8f23be76567f5b897d46b
Michael Young <mick.young1981@googlemail.com>
2021-09-28 20:03:59 +0100

    Added very basic exception handling

--- src/SFA.DAS.EmploymentCheck.Functions/Commands/CheckApprentice/CheckApprenticeCommandHandler.cs
+++ src/SFA.DAS.EmploymentCheck.Functions/Commands/CheckApprentice/CheckApprenticeCommandHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.EmploymentCheck.Functions.DataAccess;
 using SFA.DAS.EmploymentCheck.Functions.Dtos;
 using SFA.DAS.EmploymentCheck.Functions.Services;
@@ -15,29 +16,39 @@ namespace SFA.DAS.EmploymentCheck.Functions.Commands.CheckApprentice
         private readonly IEmploymentChecksRepository _repository;
         private readonly IAccountsService _accountsService;
         private readonly IHmrcService _hmrcService;
+        private readonly ILogger _logger;
 
-        public CheckApprenticeCommandHandler(IEmploymentChecksRepository repository, IAccountsService accountsService, IHmrcService hmrcService)
+        public CheckApprenticeCommandHandler(IEmploymentChecksRepository repository, IAccountsService accountsService, IHmrcService hmrcService, ILogger logger)
         {
             _repository = repository;
             _accountsService = accountsService;
             _hmrcService = hmrcService;
+            _logger = logger;
         }
 
         public async Task<Unit> Handle(CheckApprenticeCommand request, CancellationToken cancellationToken)
         {
-            var payeSchemes = await GetAccountPayeSchemes(request.Apprentice.AccountId);
-            bool checkPassed = false;
-            foreach (var payeScheme in payeSchemes)
+            try
             {
-                checkPassed = await _hmrcService.IsNationalInsuranceNumberRelatedToPayeScheme(payeScheme, request.Apprentice.NationalInsuranceNumber, request.Apprentice.StartDate, request.Apprentice.EndDate);
-                if (checkPassed)
+                var payeSchemes = await GetAccountPayeSchemes(request.Apprentice.AccountId);
+                bool checkPassed = false;
+                foreach (var payeScheme in payeSchemes)
                 {
-                    break;
+                    checkPassed = await _hmrcService.IsNationalInsuranceNumberRelatedToPayeScheme(payeScheme, request.Apprentice.NationalInsuranceNumber, request.Apprentice.StartDate, request.Apprentice.EndDate);
+                    if (checkPassed)
+                    {
+                        break;
+                    }
                 }
-            }
 
-            await StoreEmploymentCheckResult(request.Apprentice, checkPassed);
-            return Unit.Value;
+                await StoreEmploymentCheckResult(request.Apprentice, checkPassed);
+                return Unit.Value;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error handling CheckApprenticeCommand");
+                throw;
+            }
         }
 
         private async Task StoreEmploymentCheckResult(ApprenticeToVerifyDto apprentice, bool checkPassed)
