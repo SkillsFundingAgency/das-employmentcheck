0d5f8346a5f439028321dc59aeb97d6095bbdc92
Michael Young <mick.young1981@googlemail.com>
2021-09-28 08:43:39 +0100

    Added new db table.

---
+++ src/SFA.DAS.EmploymentCheck.Database/Tables/EmploymentChecks.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[EmploymentChecks]
+(
+	[Id] BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    [ULN] BIGINT NOT NULL,
+    [UKPRN] BIGINT NULL,
+    [ApprenticeshipId] BIGINT NULL,
+    [AccountId] BIGINT NULL,
+    [NINO] NCHAR(9) NOT NULL,
+    [MinDate] DATETIME NOT NULL,
+    [MaxDate] DATETIME NOT NULL,
+    [CheckType] NVARCHAR(20) NOT NULL,
+    [Result] BIT NULL,
+    [LastUpdated] DATETIME NULL,
+    [CreatedDate] DATETIME NULL DEFAULT getdate(),
+    CONSTRAINT UC_EmploymentChecks UNIQUE (Accountid, uln, ukprn, apprenticeshipid, nino, checktype)
+)
+GO
+CREATE INDEX [IX_EmploymentChecks_Column] ON [dbo].[EmploymentChecks] (uln, accountid, ukprn, checktype)
+go
+CREATE INDEX [IX_EmploymentChecks_Checks] ON [dbo].[EmploymentChecks] (checktype, uln)
