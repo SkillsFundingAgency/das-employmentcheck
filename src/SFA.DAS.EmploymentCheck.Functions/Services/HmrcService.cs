bb7dec6c61efc2fa63c0f5c8622a0e014f27a7ed
Michael Young <mick.young1981@googlemail.com>
2021-09-30 09:15:09 +0100

    Remove retry logic.

--- src/SFA.DAS.EmploymentCheck.Functions/Services/HmrcService.cs
+++ src/SFA.DAS.EmploymentCheck.Functions/Services/HmrcService.cs
@@ -10,44 +10,28 @@ namespace SFA.DAS.EmploymentCheck.Functions.Services
 {
     public class HmrcService : IHmrcService
     {
-        private AsyncPolicy _retryPolicy;
         private IApprenticeshipLevyApiClient _apprenticeshipLevyService;
         private ITokenServiceApiClient _tokenService;
 
-        public HmrcService(ITokenServiceApiClient tokenService, IApprenticeshipLevyApiClient apprenticeshipLevyService, HmrcApiSettings hmrcApiSettings)
+        public HmrcService(ITokenServiceApiClient tokenService, IApprenticeshipLevyApiClient apprenticeshipLevyService)
         {
             _tokenService = tokenService;
             _apprenticeshipLevyService = apprenticeshipLevyService;
-
-            _retryPolicy = GetRetryPolicy(hmrcApiSettings.RetryDelay);
         }
 
         public async Task<bool> IsNationalInsuranceNumberRelatedToPayeScheme(string payeScheme, string nationalInsuranceNumber, DateTime startDate, DateTime endDate)
         {
-            return await _retryPolicy.ExecuteAsync<bool>(async () =>
+            var token = await _tokenService.GetPrivilegedAccessTokenAsync();
+
+            try
             {
-                var token = await _tokenService.GetPrivilegedAccessTokenAsync();
-
-                try
-                {
-                    var response = await _apprenticeshipLevyService.GetEmploymentStatus(token.AccessCode, payeScheme, nationalInsuranceNumber, startDate, endDate);
-                    return response.Employed;
-                }
-                catch (ApiHttpException e) when (e.HttpCode == 404)
-                {
-                    return false;
-                }
-            });
-        }
-
-        private AsyncPolicy GetRetryPolicy(int retryDelay)
-        {
-            var retryPolicy = Policy.Handle<ApiHttpException>(ex => ex.HttpCode == 429 || ex.HttpCode == 408)
-                .WaitAndRetryForeverAsync(retryAttempt => TimeSpan.FromMilliseconds(retryDelay));
-
-            return retryPolicy.WrapAsync(
-                Policy.Handle<ApiHttpException>(ex => ex.HttpCode == 503 || ex.HttpCode == 500)
-                    .WaitAndRetryAsync(10, i => TimeSpan.FromMilliseconds(retryDelay)));
+                var response = await _apprenticeshipLevyService.GetEmploymentStatus(token.AccessCode, payeScheme, nationalInsuranceNumber, startDate, endDate);
+                return response.Employed;
+            }
+            catch (ApiHttpException e) when (e.HttpCode == 404)
+            {
+                return false;
+            }
         }
     }
 }
