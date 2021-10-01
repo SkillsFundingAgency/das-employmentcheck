f2296a3578a6bca4c36cfc9c6f4689094fea4d2f
Michael Young <mick.young1981@googlemail.com>
2021-10-01 11:03:24 +0100

    Added extra logging.

--- src/SFA.DAS.EmploymentCheck.Functions/Services/HmrcService.cs
+++ src/SFA.DAS.EmploymentCheck.Functions/Services/HmrcService.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using HMRC.ESFA.Levy.Api.Client;
 using HMRC.ESFA.Levy.Api.Types.Exceptions;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.TokenService.Api.Client;
 
 namespace SFA.DAS.EmploymentCheck.Functions.Services
@@ -9,12 +10,14 @@ namespace SFA.DAS.EmploymentCheck.Functions.Services
     public class HmrcService : IHmrcService
     {
         private IApprenticeshipLevyApiClient _apprenticeshipLevyService;
+        private readonly ILogger<HmrcService> _logger;
         private ITokenServiceApiClient _tokenService;
 
-        public HmrcService(ITokenServiceApiClient tokenService, IApprenticeshipLevyApiClient apprenticeshipLevyService)
+        public HmrcService(ITokenServiceApiClient tokenService, IApprenticeshipLevyApiClient apprenticeshipLevyService, ILogger<HmrcService> logger)
         {
             _tokenService = tokenService;
             _apprenticeshipLevyService = apprenticeshipLevyService;
+            _logger = logger;
         }
 
         public async Task<bool> IsNationalInsuranceNumberRelatedToPayeScheme(string payeScheme, string nationalInsuranceNumber, DateTime startDate, DateTime endDate)
@@ -23,12 +26,18 @@ namespace SFA.DAS.EmploymentCheck.Functions.Services
 
             try
             {
-                var response = await _apprenticeshipLevyService.GetEmploymentStatus(token.AccessCode, payeScheme, nationalInsuranceNumber, startDate, endDate);
+                var response = await _apprenticeshipLevyService.GetEmploymentStatus(token.AccessCode, payeScheme,
+                    nationalInsuranceNumber, startDate, endDate);
                 return response.Employed;
             }
             catch (ApiHttpException e) when (e.HttpCode == 404)
             {
                 return false;
+            }
+            catch (ApiHttpException e)
+            {
+                _logger.LogError(e, "Error calling HMRC : " + e.ResponseBody);
+                throw;
             }
         }
     }
