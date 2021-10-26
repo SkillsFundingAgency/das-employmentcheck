4cd6632fb05bee4e22223e38b2150b9e5e8d6630
Peter Williams <peter.david.williams.1991@gmail.com>
2021-10-26 16:26:03 +0100

    Added http trigger tests (log test helper not yet implemented)

---
+++ src/SFA.DAS.EmploymentCheck.Functions.Tests/AzureFunctions/Triggers/EmploymentCheckHttpTriggerTests/WhenTriggeringHttpEmploymentCheck.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Orchestrators;
+using SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Triggers;
+using Xunit;
+
+namespace SFA.DAS.EmploymentCheck.Functions.Tests.AzureFunctions.Triggers.EmploymentCheckHttpTriggerTests
+{
+    public class WhenTriggeringHttpEmploymentCheck
+    {
+        private readonly Mock<HttpRequestMessage> _request;
+        private readonly Mock<IDurableOrchestrationClient> _starter;
+        private readonly Mock<ILogger> _logger;
+
+        public WhenTriggeringHttpEmploymentCheck()
+        {
+            _request = new Mock<HttpRequestMessage>();
+            _starter = new Mock<IDurableOrchestrationClient>();
+            _logger = new Mock<ILogger>();
+        }
+
+        [Fact (Skip = "Logger test helper not yet implemented")]
+        public async void Then_The_Instance_Id_Is_Created()
+        {
+            //Arrange
+            var instanceId = "test";
+            var response = new HttpResponseMessage(HttpStatusCode.Accepted);
+
+            _starter.Setup(x => x.StartNewAsync(nameof(TestEmploymentCheckOrchestrator), null))
+                .ReturnsAsync(instanceId);
+            _starter.Setup(x => x.CreateCheckStatusResponse(_request.Object, instanceId, false))
+                .Returns(response);
+
+            //Act
+            await EmploymentCheckHttpTrigger.HttpStart(_request.Object, _starter.Object, _logger.Object);
+
+            //Assert
+            
+            //_logger.Verify(x => x.LogInformation($"Started orchestration with ID = '{instanceId}'."), Times.Once);
+        }
+
+        [Fact]
+        public async void Then_The_Status_Code_Is_Returned()
+        {
+            //Arrange
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+
+            _starter.Setup(x => x.StartNewAsync(nameof(TestEmploymentCheckOrchestrator), null))
+                .ReturnsAsync("");
+            _starter.Setup(x => x.CreateCheckStatusResponse(_request.Object, It.IsAny<string>(), false))
+                .Returns(response);
+
+            //Act
+            var result = await EmploymentCheckHttpTrigger.HttpStart(_request.Object, _starter.Object, _logger.Object);
+
+            //Assert
+            Assert.Equal(response.StatusCode, result.StatusCode);
+        }
+    }
+}
