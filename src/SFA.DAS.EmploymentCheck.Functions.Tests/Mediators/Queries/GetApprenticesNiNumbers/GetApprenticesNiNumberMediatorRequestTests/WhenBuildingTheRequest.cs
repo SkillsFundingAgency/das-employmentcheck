318648cc1d21b75abec4a0b8c2e353dfe0d77c88
Peter Williams <peter.david.williams.1991@gmail.com>
2021-11-01 12:35:51 +0000

    added get apprentice ni numbers tests

---
+++ src/SFA.DAS.EmploymentCheck.Functions.Tests/Mediators/Queries/GetApprenticesNiNumbers/GetApprenticesNiNumberMediatorRequestTests/WhenBuildingTheRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using SFA.DAS.EmploymentCheck.Functions.Application.Models.Domain;
+using SFA.DAS.EmploymentCheck.Functions.Mediators.Queries.GetApprenticesNiNumbers;
+using Xunit;
+
+namespace SFA.DAS.EmploymentCheck.Functions.Tests.Mediators.Queries.GetApprenticesNiNumbers.GetApprenticesNiNumberMediatorRequestTests
+{
+    public class WhenBuildingTheRequest
+    {
+        [Fact]
+        public void Then_The_Request_Is_Built_Successfully()
+        {
+            //Arrange
+
+            var apprentice = new Apprentice(1,
+                1,
+                "1000001", 
+                1000001,
+                1000001,
+                1, 
+                DateTime.Today.AddDays(-1),
+                DateTime.Today.AddDays(1));
+            var apprentices = new List<Apprentice> {apprentice};
+
+            //Act
+
+            var result = new GetApprenticesNiNumberMediatorRequest(apprentices);
+
+            //Assert
+
+            Assert.Equal(apprentices, result.Apprentices);
+
+        }
+    }
+}
