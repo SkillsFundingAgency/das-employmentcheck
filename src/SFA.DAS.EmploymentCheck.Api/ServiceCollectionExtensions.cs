efd727f9ed93b65dd2696b0066de7d0854730020
ruslan <ruslanss@gmail.com>
2022-01-13 11:48:38 +0000

    Add swagger UI to API proj, register MediatR handlers

---
+++ src/SFA.DAS.EmploymentCheck.Api/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.EmploymentCheck.Api.Commands.RegisterCheckCommand;
+
+namespace SFA.DAS.EmploymentCheck.Api
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddHandlers(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(RegisterCheckCommand).Assembly);
+
+            return services;
+        }
+    }
+}
