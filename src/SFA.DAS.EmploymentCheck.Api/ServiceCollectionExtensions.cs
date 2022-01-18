42fd17675c05fd39f53461f9e76e8fd9ce32687d
Peter Williams <peter.david.williams.1991@gmail.com>
2022-01-18 15:16:31 +0000

    Removed references to .Functions in service registration

--- src/SFA.DAS.EmploymentCheck.Api/ServiceCollectionExtensions.cs
+++ src/SFA.DAS.EmploymentCheck.Api/ServiceCollectionExtensions.cs
@@ -3,9 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 using SFA.DAS.EmploymentCheck.Api.Application.Services;
 using SFA.DAS.EmploymentCheck.Api.Mediators.Commands.RegisterCheckCommand;
 using SFA.DAS.EmploymentCheck.Api.Repositories;
-using SFA.DAS.EmploymentCheck.Functions.Application.Clients.EmploymentCheck;
-using SFA.DAS.EmploymentCheck.Functions.Mediators.Commands.CreateEmploymentCheckCacheRequests;
-using SFA.DAS.EmploymentCheck.Functions.Mediators.Commands.StoreEmploymentCheckResult;
 
 namespace SFA.DAS.EmploymentCheck.Api
 {
@@ -13,9 +10,7 @@ namespace SFA.DAS.EmploymentCheck.Api
     {
         public static IServiceCollection AddHandlers(this IServiceCollection services)
         {
-            services.AddMediatR(typeof(StoreEmploymentCheckResultCommand).Assembly);
             services.AddMediatR(typeof(RegisterCheckCommand).Assembly);
-            services.AddMediatR(typeof(CreateEmploymentCheckCacheCommand).Assembly);
 
             return services;
         }
@@ -23,7 +18,6 @@ namespace SFA.DAS.EmploymentCheck.Api
         public static IServiceCollection AddServices(this IServiceCollection services)
         {
             services.AddHttpClient();
-            services.AddTransient<IEmploymentCheckClient, EmploymentCheckClient>();
             services.AddTransient<IEmploymentCheckService, EmploymentCheckService>();
             services.AddTransient<IRegisterCheckCommandValidator, RegisterCheckCommandValidator>();
             
