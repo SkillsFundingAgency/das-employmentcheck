fe8cd24382a216e684cc5f7cb54bb74c9d71a598
Gary Griffiths <gary.griffiths@digital.education.gov.uk>
2022-01-26 17:06:38 +0000

    Updates for comments

--- src/SFA.DAS.EmploymentCheck.Data.IntegrationTests/Repositories/EmploymentCheck/WhenSaveEmploymentCheck.cs
+++ src/SFA.DAS.EmploymentCheck.Data.IntegrationTests/Repositories/EmploymentCheck/WhenSaveEmploymentCheck.cs
@@ -46,62 +46,6 @@ namespace SFA.DAS.EmploymentCheck.Data.IntegrationTests.Repositories
             _actual.Id.Should().BeGreaterThan(0);
         }
 
-        [Test]
-        public async Task CanSUpdate()
-        {
-            // Arrange
-            _sut = new EmploymentCheckRepository(Settings);
-            var expected = Fixture.Create<Models.EmploymentCheck>();
-
-            // Act
-            await base.Insert(expected);
-
-            _actual = (await GetAll<Models.EmploymentCheck>())
-                .Single(x => x.CorrelationId == expected.CorrelationId);
-
-            _actual.Should().BeEquivalentTo(expected,
-                opts => opts
-                    .Excluding(x => x.Id)
-                    .Excluding(x => x.CreatedOn)
-                    .Excluding(x => x.LastUpdatedOn)
-                    .Excluding(x => x.MinDate)
-                    .Excluding(x => x.MaxDate)
-                );
-
-            _actual.CreatedOn.Should().BeCloseTo(expected.CreatedOn, TimeSpan.FromSeconds(1));
-            _actual.LastUpdatedOn.Should().BeCloseTo(expected.LastUpdatedOn, TimeSpan.FromSeconds(1));
-            _actual.MinDate.Should().BeCloseTo(expected.MinDate, TimeSpan.FromSeconds(1));
-            _actual.MaxDate.Should().BeCloseTo(expected.MaxDate, TimeSpan.FromSeconds(1));
-            _actual.Id.Should().BeGreaterThan(0);
-
-            await Task.Delay(1000);
-            expected.CreatedOn = DateTime.Now;
-            expected.LastUpdatedOn = DateTime.Now;
-            expected.MinDate = DateTime.Now;
-            expected.MaxDate = DateTime.Now;
-
-            await _sut.InsertOrUpdate(expected);
-
-            // Assert
-            _actual = (await GetAll<Models.EmploymentCheck>())
-                .Single(x => x.CorrelationId == expected.CorrelationId);
-
-            _actual.Should().BeEquivalentTo(expected,
-                opts => opts
-                    .Excluding(x => x.Id)
-                    .Excluding(x => x.CreatedOn)
-                    .Excluding(x => x.LastUpdatedOn)
-                    .Excluding(x => x.MinDate)
-                    .Excluding(x => x.MaxDate)
-                );
-
-            _actual.CreatedOn.Should().BeCloseTo(expected.CreatedOn, TimeSpan.FromSeconds(1));
-            _actual.LastUpdatedOn.Should().BeCloseTo(expected.LastUpdatedOn, TimeSpan.FromSeconds(1));
-            _actual.MinDate.Should().BeCloseTo(expected.MinDate, TimeSpan.FromSeconds(1));
-            _actual.MaxDate.Should().BeCloseTo(expected.MaxDate, TimeSpan.FromSeconds(1));
-            _actual.Id.Should().BeGreaterThan(0);
-        }
-
         [TearDown]
         public async Task CleanUp()
         {
