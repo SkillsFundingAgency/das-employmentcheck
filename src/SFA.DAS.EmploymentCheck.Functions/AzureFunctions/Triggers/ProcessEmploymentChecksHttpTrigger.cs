14fc500b477b1d72744b316c0620def41a563f0a
Peter Williams <peter.david.williams.1991@gmail.com>
2022-01-26 08:28:21 +0000

    Made ProcessEmploymentChecksHttpTrigger singleton, added tests

--- src/SFA.DAS.EmploymentCheck.Functions/AzureFunctions/Triggers/ProcessEmploymentChecksHttpTrigger.cs
+++ src/SFA.DAS.EmploymentCheck.Functions/AzureFunctions/Triggers/ProcessEmploymentChecksHttpTrigger.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Azure.WebJobs;
+﻿using System.Linq;
+using System.Net;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
@@ -10,19 +12,42 @@ namespace SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Triggers
 {
     public static class ProcessEmploymentChecksHttpTrigger
     {
+        private const string InstanceIdPrefix = "EmploymentCheck-";
+
         [FunctionName("ProcessApprenticeEmploymentChecksHttpTrigger")]
         public static async Task<HttpResponseMessage> HttpStart(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "orchestrators/ProcessApprenticeEmploymentChecksOrchestrator")] HttpRequestMessage req,
             [DurableClient] IDurableOrchestrationClient starter,
             ILogger log)
         {
-            log.LogInformation($"Triggering {nameof(ProcessEmploymentCheckRequestsWithRateLimiterOrchestrator)}");
+            log.LogInformation("Checking for running instances of ProcessEmploymentCheckRequestsWithRateLimiterOrchestrator");
 
-            string instanceId = await starter.StartNewAsync(nameof(ProcessEmploymentCheckRequestsWithRateLimiterOrchestrator), null);
+            var existingInstances = await starter.ListInstancesAsync(new OrchestrationStatusQueryCondition
+            {
+                InstanceIdPrefix = InstanceIdPrefix,
+                RuntimeStatus = new[]
+                {
+                    OrchestrationRuntimeStatus.Pending,
+                    OrchestrationRuntimeStatus.Running,
+                    OrchestrationRuntimeStatus.ContinuedAsNew
+                }
+            }, System.Threading.CancellationToken.None);
 
-            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+            if (!existingInstances.DurableOrchestrationState.Any())
+            {
+                log.LogInformation($"Triggering {nameof(ProcessEmploymentCheckRequestsWithRateLimiterOrchestrator)}");
+                
+                string instanceId = await starter.StartNewAsync(nameof(ProcessEmploymentCheckRequestsWithRateLimiterOrchestrator), null);
+                
+                log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+                
+                return starter.CreateCheckStatusResponse(req, instanceId);
+            }
 
-            return starter.CreateCheckStatusResponse(req, instanceId);
+            return new HttpResponseMessage(HttpStatusCode.Conflict)
+            {
+                Content = new StringContent($"An instance of {nameof(ProcessEmploymentCheckRequestsWithRateLimiterOrchestrator)} is already running."),
+            };
         }
     }
 }
