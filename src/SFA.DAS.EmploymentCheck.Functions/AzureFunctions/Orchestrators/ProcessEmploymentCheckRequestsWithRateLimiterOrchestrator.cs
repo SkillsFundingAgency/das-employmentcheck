9f385ab5c985fbc26f26dc8c05390cfb87a8adc5
Gary Griffiths <gary.griffiths@digital.education.gov.uk>
2022-02-02 14:20:08 +0000

    Removed commented out code

--- src/SFA.DAS.EmploymentCheck.Functions/AzureFunctions/Orchestrators/ProcessEmploymentCheckRequestsWithRateLimiterOrchestrator.cs
+++ src/SFA.DAS.EmploymentCheck.Functions/AzureFunctions/Orchestrators/ProcessEmploymentCheckRequestsWithRateLimiterOrchestrator.cs
@@ -40,9 +40,6 @@ namespace SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Orchestrators
                     // Do the employment status check on this request
                     var result = await context.CallActivityAsync<EmploymentCheckCacheRequest>(nameof(GetHmrcLearnerEmploymentStatusActivity), employmentCheckCacheRequest);
 
-                    // Save the employment status back to the database
-                    //await context.CallActivityAsync(nameof(StoreEmploymentCheckResultActivity), result);
-
                     // Execute RateLimiter
                     var delayTimeSpan = await context.CallActivityAsync<TimeSpan>(nameof(AdjustEmploymentCheckRateLimiterOptionsActivity), result);
 
