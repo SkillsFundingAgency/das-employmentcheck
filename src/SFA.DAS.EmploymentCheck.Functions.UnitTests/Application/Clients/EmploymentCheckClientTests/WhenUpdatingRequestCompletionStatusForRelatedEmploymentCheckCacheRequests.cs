7f4272823b72143f017e47f85bd5d4efd9662a7f
Gary Griffiths <gary.griffiths@digital.education.gov.uk>
2022-02-15 13:44:44 +0000

    Uncommented the commented out tests

--- src/SFA.DAS.EmploymentCheck.Functions.UnitTests/Application/Clients/EmploymentCheckClientTests/WhenUpdatingRequestCompletionStatusForRelatedEmploymentCheckCacheRequests.cs
+++ src/SFA.DAS.EmploymentCheck.Functions.UnitTests/Application/Clients/EmploymentCheckClientTests/WhenUpdatingRequestCompletionStatusForRelatedEmploymentCheckCacheRequests.cs
@@ -1,44 +1,44 @@
-﻿//using AutoFixture;
-//using Moq;
-//using NUnit.Framework;
-//using SFA.DAS.EmploymentCheck.Functions.Application.Clients.EmploymentCheck;
-//using SFA.DAS.EmploymentCheck.Functions.Application.Models;
-//using SFA.DAS.EmploymentCheck.Functions.Application.Services.EmploymentCheck;
-//using System.Threading.Tasks;
+﻿using AutoFixture;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmploymentCheck.Functions.Application.Clients.EmploymentCheck;
+using SFA.DAS.EmploymentCheck.Functions.Application.Models;
+using SFA.DAS.EmploymentCheck.Functions.Application.Services.EmploymentCheck;
+using System.Threading.Tasks;
 
-//namespace SFA.DAS.EmploymentCheck.Functions.UnitTests.Application.Clients.EmploymentCheckClientTests
-//{
-//    public class WhenUpdatingRequestCompletionStatusForRelatedEmploymentCheckCacheRequests
-//    {
+namespace SFA.DAS.EmploymentCheck.Functions.UnitTests.Application.Clients.EmploymentCheckClientTests
+{
+    public class WhenUpdatingRequestCompletionStatusForRelatedEmploymentCheckCacheRequests
+    {
 
-//        private readonly Fixture _fixture;
-//        private readonly Mock<IEmploymentCheckService> _employmentCheckServiceMock = new Mock<IEmploymentCheckService>();
-//        private readonly EmploymentCheckClient _sut;
+        private readonly Fixture _fixture;
+        private readonly Mock<IEmploymentCheckService> _employmentCheckServiceMock = new Mock<IEmploymentCheckService>();
+        private readonly EmploymentCheckClient _sut;
 
-//        public WhenUpdatingRequestCompletionStatusForRelatedEmploymentCheckCacheRequests()
-//        {
-//            _fixture = new Fixture();
-//            _sut = new EmploymentCheckClient(_employmentCheckServiceMock.Object);
-//        }
+        public WhenUpdatingRequestCompletionStatusForRelatedEmploymentCheckCacheRequests()
+        {
+            _fixture = new Fixture();
+            _sut = new EmploymentCheckClient(_employmentCheckServiceMock.Object);
+        }
 
-//        [Test]
-//        public async Task Then_The_EmploymentCheckService_Is_Called()
-//        {
-//            // Arrange
-//            var employmentCheckCacheRequest = _fixture
-//                .Build<EmploymentCheckCacheRequest>()
-//                .Create();
+        [Test]
+        public async Task Then_The_EmploymentCheckService_Is_Called()
+        {
+            // Arrange
+            var employmentCheckCacheRequest = _fixture
+                .Build<EmploymentCheckCacheRequest>()
+                .Create();
 
-//            _employmentCheckServiceMock
-//                .Setup(x => x.UpdateRelatedRequests(employmentCheckCacheRequest));
+            _employmentCheckServiceMock
+                .Setup(x => x.UpdateRelatedRequests(employmentCheckCacheRequest));
 
-//            // Act
-//            await _sut
-//                .UpdateRequestCompletionStatusForRelatedEmploymentCheckCacheRequests(employmentCheckCacheRequest);
+            // Act
+            await _sut
+                .UpdateRequestCompletionStatusForRelatedEmploymentCheckCacheRequests(employmentCheckCacheRequest);
 
-//            // Assert
-//            _employmentCheckServiceMock
-//                .Verify(x => x.UpdateRelatedRequests(employmentCheckCacheRequest), Times.AtLeastOnce);
-//        }
-//    }
-//}
+            // Assert
+            _employmentCheckServiceMock
+                .Verify(x => x.UpdateRelatedRequests(employmentCheckCacheRequest), Times.AtLeastOnce);
+        }
+    }
+}
