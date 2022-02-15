9f978be5e546c3b8f920fa0a52f77c50928b797d
Gary Griffiths <gary.griffiths@digital.education.gov.uk>
2022-02-15 20:46:22 +0000

    Added Create and Process orchestrator tests

---
+++ src/SFA.DAS.EmploymentCheck.Functions.UnitTests/AzureFunctions/Orchestrators/CreateEmploymentCheckCacheRequestsOrchestratorTests/WhenRunningCreateEmploymentCheckCacheRequestsOrchestrator.cs
@@ -0,0 +1,57 @@
+﻿using AutoFixture;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmploymentCheck.Functions.Application.Models;
+using SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Activities;
+using SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Orchestrators;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models = SFA.DAS.EmploymentCheck.Functions.Application.Models;
+
+namespace SFA.DAS.EmploymentCheck.Functions.UnitTests.AzureFunctions.Orchestrators.CreateEmploymentCheckCacheRequestsOrchestratorTests
+{
+    public class WhenRunningCreateEmploymentCheckCacheRequestsOrchestrator
+    {
+        private Fixture _fixture;
+        private Mock<IDurableOrchestrationContext> _context;
+        private Mock<ILogger<CreateEmploymentCheckCacheRequestsOrchestrator>> _logger;
+        private IList<Models.EmploymentCheck> _employmentChecks;
+        private CreateEmploymentCheckCacheRequestsOrchestrator _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+            _context = new Mock<IDurableOrchestrationContext>();
+            _logger = new Mock<ILogger<CreateEmploymentCheckCacheRequestsOrchestrator>>();
+
+            _employmentChecks = new List<Models.EmploymentCheck>
+            {
+                _fixture.Create<Models.EmploymentCheck>()
+            };
+
+            _sut = new CreateEmploymentCheckCacheRequestsOrchestrator(_logger.Object);
+        }
+
+        [Test]
+        public async Task Then_The_SubOrchestrator_Is_Called()
+        {
+            // Arrange
+            _context
+                .Setup(a => a.CallActivityAsync<IList<Models.EmploymentCheck>>(nameof(GetEmploymentChecksBatchActivity), It.IsAny<object>()))
+                .ReturnsAsync(_employmentChecks);
+
+            _context
+                .Setup(o => o.CallSubOrchestratorAsync<EmploymentCheckCacheRequest>(nameof(CreateEmploymentCheckCacheRequestOrchestrator), It.IsAny<Models.EmploymentCheck>()));
+
+            // Act
+            await _sut.CreateEmploymentCheckRequestsTask(_context.Object);
+
+            // Assert
+            _context.Verify(a => a.CallActivityAsync<IList<Models.EmploymentCheck>>(nameof(GetEmploymentChecksBatchActivity), It.IsAny<object>()), Times.Once);
+            _context.Verify(o => o.CallSubOrchestratorAsync<EmploymentCheckCacheRequest>(nameof(CreateEmploymentCheckCacheRequestOrchestrator), It.IsAny<Models.EmploymentCheck>()), Times.Once);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmploymentCheck.Functions.UnitTests/AzureFunctions/Orchestrators/ProcessEmploymentCheckRequestsWithRateLimiterOrchestratorTests/WhenRunningProcessEmploymentCheckRequestsWithRateLimiterOrchestrator.cs
@@ -0,0 +1,60 @@
+﻿using AutoFixture;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmploymentCheck.Functions.Application.Models;
+using SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Activities;
+using SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Orchestrators;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models = SFA.DAS.EmploymentCheck.Functions.Application.Models;
+
+namespace SFA.DAS.EmploymentCheck.Functions.UnitTests.AzureFunctions.Orchestrators.ProcessEmploymentCheckRequestsWithRateLimiterOrchestratorTests
+{
+    public class WhenRunningProcessEmploymentCheckRequestsWithRateLimiterOrchestrator
+    {
+        private Fixture _fixture;
+        private Mock<IDurableOrchestrationContext> _context;
+        private Mock<ILogger<ProcessEmploymentCheckRequestsWithRateLimiterOrchestrator>> _logger;
+
+        private EmploymentCheckCacheRequest employmentCheckCacheRequest;
+        private IList<Models.EmploymentCheck> _employmentChecks;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+            _context = new Mock<IDurableOrchestrationContext>();
+            _logger = new Mock<ILogger<ProcessEmploymentCheckRequestsWithRateLimiterOrchestrator>>();
+
+            employmentCheckCacheRequest = _fixture.Create<EmploymentCheckCacheRequest>();
+            _employmentChecks = new List<Models.EmploymentCheck> { _fixture.Create<Models.EmploymentCheck>() };
+            _employmentChecks = new List<Models.EmploymentCheck> { _fixture.Create<Models.EmploymentCheck>() };
+        }
+
+        [Test]
+        public async Task Then_The_Activities_Are_Called()
+        {
+            // Arrange
+            var sut = new ProcessEmploymentCheckRequestsWithRateLimiterOrchestrator(_logger.Object);
+
+            _context
+                .Setup(a => a.CallActivityAsync<EmploymentCheckCacheRequest>(nameof(GetEmploymentCheckCacheRequestActivity), null))
+                .ReturnsAsync(employmentCheckCacheRequest);
+
+            _context
+                .Setup(a => a.CallActivityAsync<EmploymentCheckCacheRequest>(nameof(GetHmrcLearnerEmploymentStatusActivity), _employmentChecks))
+                .ReturnsAsync(employmentCheckCacheRequest);
+
+            _context
+                .Setup(a => a.CallActivityAsync(nameof(CreateEmploymentCheckCacheRequestsActivity), It.IsAny<EmploymentCheckData>()));
+
+            // Act
+            await sut.ProcessEmploymentChecksWithRateLimiterOrchestratorTask(_context.Object);
+
+            // Assert
+            _context.Verify(a => a.CallActivityAsync<EmploymentCheckCacheRequest>(nameof(GetEmploymentCheckCacheRequestActivity), null), Times.Once);
+        }
+    }
+}
