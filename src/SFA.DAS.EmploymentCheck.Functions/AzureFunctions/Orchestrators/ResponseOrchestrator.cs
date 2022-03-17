bce4a24a519ea19f4468f4400e6f9c890e371ca7
Gary Griffiths <gary.griffiths@digital.education.gov.uk>
2022-03-17 10:52:02 +0000

    WIP - basic orchestrator with call to get the response employment check

---
+++ src/SFA.DAS.EmploymentCheck.Functions/AzureFunctions/Orchestrators/ResponseOrchestrator.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Activities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using SFA.DAS.EmploymentCheck.Data.Models;
+
+namespace SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Orchestrators
+{
+    public class ResponseOrchestrator
+    {
+        private readonly ILogger<ResponseOrchestrator> _logger;
+
+        public ResponseOrchestrator(
+            ILogger<ResponseOrchestrator> logger)
+        {
+            _logger = logger;
+        }
+
+        [FunctionName(nameof(ResponseOrchestrator))]
+        public async Task ResponseOrchestratorTask([OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            try
+            {
+                var employmentCheck = await context.CallActivityAsync<Data.Models.EmploymentCheck>(nameof(GetResponseEmploymentCheckActivity), null);
+                if (employmentCheck != null)
+                {
+                    //await context.CallActivityAsync<EmploymentCheckCacheRequest>(nameof(GetHmrcLearnerEmploymentStatusActivity), employmentCheck);
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"\n\n{nameof(ResponseOrchestrator)} Exception caught: {e.Message}. {e.StackTrace}");
+            }
+            finally
+            {
+                //context.ContinueAsNew(null);
+            }
+
+        }
+    }
+}
