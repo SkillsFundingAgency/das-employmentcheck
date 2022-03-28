735168df0a7c9c9cbdbe003561b84f9c5536be7c
Gary Griffiths <gary.griffiths@digital.education.gov.uk>
2022-03-28 17:16:23 +0100

    Added additional unit tests

--- src/SFA.DAS.EmploymentCheck.Functions.UnitTests/AzureFunctions/Orchestrators/ResetEmploymentChecksMessageSentDateOrchestratorTests/WhenResetEmploymentChecksMessageSentDateOrchestratorIsTriggered.cs
+++ src/SFA.DAS.EmploymentCheck.Functions.UnitTests/AzureFunctions/Orchestrators/ResetEmploymentChecksMessageSentDateOrchestratorTests/WhenResetEmploymentChecksMessageSentDateOrchestratorIsTriggered.cs
@@ -1,10 +1,12 @@
 ﻿using AutoFixture;
+using FluentAssertions;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Activities;
 using SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Orchestrators;
 using SFA.DAS.EmploymentCheck.Queries;
+using System;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.EmploymentCheck.Functions.UnitTests.AzureFunctions.Orchestrators.ResetEmploymentChecksMessageSentDateOrchestratorTests
@@ -29,6 +31,22 @@ namespace SFA.DAS.EmploymentCheck.Functions.UnitTests.AzureFunctions.Orchestrato
 
             // Assert
             _mockOrchestrationContext.Verify(c => c.CallActivityAsync<long>(nameof(ResetEmploymentChecksMessageSentDateActivity), It.IsAny<IQueryDispatcher>()), Times.Once);
+        }
+
+
+        [Test]
+        public async Task Then_Error_Is_Returned__in_case_of_exception()
+        {
+            // Arrange
+            _mockOrchestrationContext.Setup(x => x.CallActivityAsync<long>(nameof(ResetEmploymentChecksMessageSentDateActivity), It.IsAny<IQueryDispatcher>()))
+                .Throws(new Exception());
+
+            // Act
+            var result = await _sut.ResetEmploymentChecksMessageSentDateTask(_mockOrchestrationContext.Object);
+
+            // Assert
+            _mockOrchestrationContext.Verify(c => c.CallActivityAsync<long>(nameof(ResetEmploymentChecksMessageSentDateActivity), It.IsAny<IQueryDispatcher>()), Times.Once);
+            result.Should().Contain("Exception");
         }
     }
 }
--- src/SFA.DAS.EmploymentCheck.Queries.UnitTests/ResetEmploymentChecksMessageSentDate/WhenHandlingTheRequest.cs
+++ src/SFA.DAS.EmploymentCheck.Queries.UnitTests/ResetEmploymentChecksMessageSentDate/WhenHandlingTheRequest.cs
@@ -1,4 +1,5 @@
 ﻿using AutoFixture;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.EmploymentCheck.Application.Services.EmploymentCheck;
@@ -47,6 +48,71 @@ namespace SFA.DAS.EmploymentCheck.Queries.UnitTests.ResetEmploymentChecksMessage
 
             // Assert
             Assert.AreEqual(employmentCheckMessageSentData, request.EmploymentCheckMessageSentData);
+        }
+
+        [Test]
+        public async Task Then_Guard_Throws_Exception_When_Input_Is_Null()
+        {
+            // Arrange
+            var request = new ResetEmploymentChecksMessageSentDateQueryRequest(null);
+            var employmentCheckMessageSentData = "CorrelationId=E269AE35-5A56-4DC8-A478-170E88280C31";
+
+            // Act
+            string error = string.Empty;
+            try
+            {
+                await _sut.Handle(null);
+            }
+            catch(Exception ex)
+            {
+                error = ex.Message;
+            }
+
+            // Assert
+            error.Should().Contain("Value cannot be null");
+        }
+
+        [Test]
+        public async Task Then_ResetEmploymentChecksMessageSentDate_Is_Called_With_CorrelationId()
+        {
+            // Arrange
+            var employmentCheckMessageSentData = "correlationid=E269AE35-5A56-4DC8-A478-170E88280C31";
+            var request = new ResetEmploymentChecksMessageSentDateQueryRequest(employmentCheckMessageSentData);
+            var args = request.EmploymentCheckMessageSentData;
+            var correlationIdString = args.Split("=")[1];
+            Guid CorrelationId = Guid.Parse(correlationIdString);
+
+            _serviceMock
+                .Setup(x => x.ResetEmploymentChecksMessageSentDate(CorrelationId))
+                .ReturnsAsync(await Task.FromResult(1L));
+
+            // Act
+            var result = await _sut.Handle(request);
+
+            // Assert
+            result.UpdatedRowsCount.Should().Be(1);
+        }
+
+        [Test]
+        public async Task Then_ResetEmploymentChecksMessageSentDate_Is_Called_With_MessageSentDate_Range()
+        {
+            // Arrange
+            var employmentCheckMessageSentData = "MessageSentFromDate=2022-03-23&MessageSentToDate=2022-03-25";
+            var request = new ResetEmploymentChecksMessageSentDateQueryRequest(employmentCheckMessageSentData);
+            var args = request.EmploymentCheckMessageSentData;
+            var temp = args.Split("=")[1];
+            var messageSentFromDate = Convert.ToDateTime(temp.Split('&')[0]);
+            var messageSentToDate = Convert.ToDateTime(args.Split("=")[2]);
+
+            _serviceMock
+                .Setup(x => x.ResetEmploymentChecksMessageSentDate(messageSentFromDate, messageSentToDate))
+                .ReturnsAsync(await Task.FromResult(1L));
+
+            // Act
+            var result = await _sut.Handle(request);
+
+            // Assert
+            result.UpdatedRowsCount.Should().Be(1);
         }
     }
 }
