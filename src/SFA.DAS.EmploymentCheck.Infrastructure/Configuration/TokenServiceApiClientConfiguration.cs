860d6e5d5c5d966adbcc2f54ac44a0cd16328d38
ruslanss <ruslanss@gmail.com>
2022-03-29 16:36:26 +0300

    ⚠️ HTB-339 Remove unused properties from TokenServiceApiClientConfiguration (#143)

--- src/SFA.DAS.EmploymentCheck.Infrastructure/Configuration/TokenServiceApiClientConfiguration.cs
+++ src/SFA.DAS.EmploymentCheck.Infrastructure/Configuration/TokenServiceApiClientConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using SFA.DAS.TokenService.Api.Client;
+﻿using SFA.DAS.TokenService.Api.Client;
 
 namespace SFA.DAS.EmploymentCheck.Infrastructure.Configuration
 {
@@ -10,32 +9,5 @@ namespace SFA.DAS.EmploymentCheck.Infrastructure.Configuration
         public string ClientSecret { get; set; }
         public string IdentifierUri { get; set; }
         public string Tenant { get; set; }
-        public string CertificateThumbprint { get; set; }
-        public X509Certificate TokenCertificate
-        {
-            get
-            {
-                var store = new X509Store(StoreLocation.LocalMachine);
-                store.Open(OpenFlags.ReadOnly);
-                try
-                {
-                    var thumbprint = CertificateThumbprint;
-
-                    if (string.IsNullOrWhiteSpace(thumbprint))
-                    {
-                        return null;
-                    }
-
-                    var certificates = store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
-
-                    return certificates[0];
-                }
-                finally
-                {
-                    store.Close();
-                }
-            }
-            set { }
-        }
     }
 }
