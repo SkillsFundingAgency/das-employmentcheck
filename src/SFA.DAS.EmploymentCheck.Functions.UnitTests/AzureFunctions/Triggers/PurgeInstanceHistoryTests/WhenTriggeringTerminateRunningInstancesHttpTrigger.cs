a7600100c650c0d5263f65c5369bcd1f9d4b35d7
ruslan <ruslanss@gmail.com>
2022-03-31 17:40:54 +0100

    Add TerminateRunningInstances and PurgeInstanceHistory orchestrators as post-deployment curl job

---
+++ src/SFA.DAS.EmploymentCheck.Functions.UnitTests/AzureFunctions/Triggers/PurgeInstanceHistoryTests/WhenTriggeringTerminateRunningInstancesHttpTrigger.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture;
+using DurableTask.Core;
+using FluentAssertions;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Triggers;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmploymentCheck.Functions.UnitTests.AzureFunctions.Triggers.PurgeInstanceHistoryTests
+{
+    public class WhenTriggeringPurgeInstanceHistoryHttpTrigger
+    {
+        private Mock<HttpRequestMessage> _request;
+        private Mock<IDurableOrchestrationClient> _starter;
+        private Mock<ILogger> _logger;
+        private Fixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture();
+            _request = new Mock<HttpRequestMessage>();
+            _starter = new Mock<IDurableOrchestrationClient>();
+            _logger = new Mock<ILogger>();
+        }
+
+        [Test]
+        public async Task Then_all_orchestrator_instance_history_is_cleared()
+        {
+            // Arrange
+            var allStatuses = Enum.GetValues(typeof(OrchestrationStatus)).Cast<OrchestrationStatus>();
+            var expected = _fixture.Create<PurgeHistoryResult>();
+            _starter.Setup(s => s.PurgeInstanceHistoryAsync(DateTime.MinValue, null, allStatuses))
+                .ReturnsAsync(expected);
+
+            // Act
+            var result = await PurgeInstanceHistoryHttpTrigger.HttpStart(_request.Object, _starter.Object, _logger.Object);
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            result.Content.ReadAsStringAsync().Result.Should().Be($"PurgeInstanceHistoryHttpTrigger: cleanup done, {expected.InstancesDeleted} instances deleted.");
+            _starter.VerifyAll();
+        }
+    }
+}
