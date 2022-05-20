31c246c045321afbc4ee7e8955aa1115848fe50b
ZackTechLtd <za1012001@yahoo.co.uk>
2022-05-20 11:20:18 +0100

    Update WhenCreatingNLogConfiguration to the latest version

--- src/SFA.DAS.EmploymentCheck.Api.UnitTests/Configuration/WhenCreatingNLogConfiguration.cs
+++ src/SFA.DAS.EmploymentCheck.Api.UnitTests/Configuration/WhenCreatingNLogConfiguration.cs
@@ -1,44 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using AutoFixture;
-using FluentAssertions;
+﻿using FluentAssertions;
 using NLog;
 using NUnit.Framework;
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ApplicationInsights.NLogTarget;
+using NLog.Targets;
+using SFA.DAS.NLog.Targets.Redis.DotNetCore;
 
 namespace SFA.DAS.EmploymentCheck.Api.UnitTests.Configuration
 {
     public class WhenCreatingNLogConfiguration
     {
+        private const string AppName = "das-employment-check-api";
+
         [Test]
-        public void Then_Create_NLogConfiguration()
+        public void Then_LocalFileTarget_Is_Created_For_Local_Environment()
         {
             // Arrange
-            var configuration = new NLogConfiguration();
-            
-            // Act
-            configuration.ConfigureNLog();
-            
-            // Assert
-            LogManager.Configuration.Should().NotBeNull();
-
-
-
-        }
-        [Test]
-        public void Then_Create_With_Environment_Variable_TEST_NLogConfiguration()
-        {
-            // Arrange
-            var configuration = new NLogConfiguration();
-            Environment.SetEnvironmentVariable("EnvironmentName", "TEST");
-
-            // Act
-            configuration.ConfigureNLog();
             Environment.SetEnvironmentVariable("EnvironmentName", "LOCAL");
+            var configuration = new NLogConfiguration();
+            var expected = new FileTarget("Disk")
+            {
+                FileName = Path.Combine(Directory.GetCurrentDirectory(), $"logs\\{AppName}.${{shortdate}}.log"),
+                Layout = "${longdate} [${uppercase:${level}}] [${logger}] - ${message} ${onexception:${exception:format=tostring}}"
+            };
+
+            // Act
+            configuration.ConfigureNLog();
 
             // Assert
-            LogManager.Configuration.Should().NotBeNull();
+            LogManager.Configuration.AllTargets.Should().HaveCount(1);
+            LogManager.Configuration.AllTargets.First().Should().BeEquivalentTo(expected);
         }
 
+        [Test]
+        public void Then_RedisTarget_Is_Created_For_NonLocal_Environment()
+        {
+            // Arrange
+            Environment.SetEnvironmentVariable("EnvironmentName", "TEST");
+            var configuration = new NLogConfiguration();
+            var expected = new RedisTarget
+            {
+                Name = "RedisLog",
+                AppName = AppName,
+                EnvironmentKeyName = "EnvironmentName",
+                ConnectionStringName = "LoggingRedisConnectionString",
+                IncludeAllProperties = true,
+                Layout = "${message}"
+            };
+
+            // Act
+            configuration.ConfigureNLog();
+
+            // Assert
+            LogManager.Configuration.AllTargets.Should().HaveCount(2);
+            LogManager.Configuration.AllTargets.First().Should().BeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void Then_ApplicationInsightsTarget_Is_Created_For_NonLocal_Environment()
+        {
+            // Arrange
+            Environment.SetEnvironmentVariable("EnvironmentName", "TEST");
+            var configuration = new NLogConfiguration();
+            var expected = new ApplicationInsightsTarget
+            {
+                Name = "AppInsightsLog"
+            };
+
+            // Act
+            configuration.ConfigureNLog();
+
+            // Assert
+            LogManager.Configuration.AllTargets.Should().HaveCount(2);
+            LogManager.Configuration.AllTargets.Last().Should().BeEquivalentTo(expected);
+        }
     }
 }
