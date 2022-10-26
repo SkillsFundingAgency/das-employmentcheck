8995ba15c32c5e5d3a756051138b44ff7d0c6d86
David-B-Read <david.read@digital.education.gov.uk>
2022-10-26 20:47:13 +0100

    EI-1956 add local and Azure NLog config for API

--- src/SFA.DAS.EmploymentCheck.Api/Program.cs
+++ src/SFA.DAS.EmploymentCheck.Api/Program.cs
@@ -1,12 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SFA.DAS.EmploymentCheck.Api
 {
