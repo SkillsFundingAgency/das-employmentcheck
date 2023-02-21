5371713ce7727eceb2e4fa7ce3bc311da072d26d
mandrici <MihailAndrici@gmail.com>
2023-02-21 19:12:14 +0000

    Add unit test to assert the  HMRC API sanitizer is called for all telemetry processed

---
+++ src/SFA.DAS.EmploymentCheck.Functions.UnitTests/AzureFunctions/Telemetry/WhenInitializingTelemetry.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmploymentCheck.Functions.AzureFunctions.Telemetry;
+
+namespace SFA.DAS.EmploymentCheck.Functions.UnitTests.AzureFunctions.Telemetry
+{
+    public class WhenInitializingTelemetry
+    {
+        [Test]
+        public void Then_The_HMRC_API_Telemetry_Sanitizer_Is_Called()
+        {
+            //Arrange
+            var sanitizer = new Mock<IHmrcApiTelemetrySanitizer>();
+            var sut = new TelemetryIntializer(sanitizer.Object);
+
+            //Act
+            sut.Initialize(It.IsAny<ITelemetry>());
+
+            //Assert
+            sanitizer.Verify(x => x.ProcessHmrcApiTelemetry(It.IsAny<ITelemetry>()), Times.Once);
+        }
+    }
+}
